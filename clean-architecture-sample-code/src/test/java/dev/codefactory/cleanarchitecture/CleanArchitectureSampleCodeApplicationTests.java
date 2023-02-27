59934651311dcddfeaef3d6fafcb73d5ac4a5a86
LM Bibera <secret.transaction@gmail.com>
2023-02-27 16:24:38 +0800

    Generated Project for Spring Boot

---
+++ clean-architecture-sample-code/src/test/java/dev/codefactory/cleanarchitecture/CleanArchitectureSampleCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codefactory.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureSampleCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
